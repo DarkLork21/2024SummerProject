78cacbe99e8809f2730579096e835d23414fe8db
DarkLork21 <gnjt13@gmail.com>
2024-07-09 15:11:15 -0500

    whut

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   
   return ( <div>
-    
+    <h3>Why ghost pepper is better then birds eye in the binding of isaac</h3>
+    <hr></hr>
+   <ul>
+    <li>Pros</li> 
+   </ul>
+   <ol><li>Guarenteed projectile deletion</li>
+   <li>Guarenteed value</li>
+   <li>better color</li>
+   </ol>
+   <hr></hr>
+   <ul>
+   <li>Cons</li> 
+   </ul>
+   <ol><li>Fire has limited lifespan</li>
+   </ol>
+   <hr></hr>
+   <h2>Explanation</h2>
+   <p>Ghost pepper is the same as birds eye with one key difference, 
+    birds eye will last until enough projectiles hit it to put the fire out whilst ghost pepper last about 3 seconds before going away. 
+    That may seem like that alone makes it worse, however on average birds eye will only destroy around 3 tears max while ghost pepper can delete all projectiles until it fissles away, 
+    which means when you hit high enough luck it tends to attain much higher value over birds eye </p>
   </div>
   )
 }
